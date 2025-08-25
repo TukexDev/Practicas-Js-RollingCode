57c20739444be8a9156ff9ad3ded345ba17d2f92
Matias Rearte <devmatiasrearte@gmail.com>
2025-08-24 23:56:11 -0300

    primer commit, ejercicios tp1

---
+++ Js - ejercicios 1/index.js
@@ -0,0 +1,64 @@
+// Ejercicio 1
+
+// alert("Hola mi raaaaay! 🤘");
+
+// Ejercicio 2
+
+// document.write("Hello World! (o en tucumano) Hola mi raaaaaaay!");
+
+// Ejercicio 3
+
+// console.log(3 + 5);
+
+// Ejercico 4 
+
+// let nombre = prompt("ingrese un nombre");
+// document.write("Hola " + nombre + "!");
+
+// Ejercico 5
+
+// let num1 = +prompt("ingrese un numero");
+// let num2 = +prompt("ingrese otro numero");
+
+// document.write(num1 + num2);
+
+// Ejercicio 6
+
+// let num1 = +prompt("ingrese un número")
+// let num2 = +prompt("ingrese otro número")
+
+// if(num1 > num2) {
+//     document.write(`El ${num1} es el número más grande`);
+// } else if (num2 > num1){
+//     document.write(`El ${num2} es el número más grande`);
+// }
+// else{
+//     document.write(`Los dos son iguales mi rey`);
+// }
+
+// Ejercicio 7
+
+// let num1 = +prompt("ingrese un número")
+// let num2 = +prompt("ingrese otro número")
+// let num3 = +prompt("ingrese un último número")
+
+// if(num1 > num2 && num3) {
+//     document.write(`El ${num1} es el número más grande`);
+// } else if (num2 > num1 && num3){
+//     document.write(`El ${num2} es el número más grande`);
+// } else if (num3 > num1 && num2){
+//     document.write(`El ${num3} es el número más grande`);
+// }
+// else{
+//     document.write(`Los tres son iguales mi rey`);
+// }
+
+// Ejercicio 8
+
+// let num1 = +prompt("ingrese un número")
+
+// if (num1 % 2 === 0){
+//     document.write(`El ${num1} es divisible por 2`);
+// } else {
+//     document.write(`El ${num1} no es divisible por 2`);
+// }
