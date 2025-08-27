ee65d23bd0b9ff5d887e7570906f2c968d7f0d27
Matias Rearte <devmatiasrearte@gmail.com>
2025-08-27 20:29:23 -0300

    avance tp3 - inicio tp4

--- Js - ejercicios 2/index.js
+++ Js - ejercicios 2/index.js
@@ -154,3 +154,23 @@
 // document.write("Fin del programa");
 
 ///////////////// Ejercicio 6 /////////////////
+
+
+
+///////////////// Ejercicio 9 /////////////////
+
+//9- Crea un script que escriba los números del 1 al 500, que indique cuáles son múltiplos de 4 y de 9 y que cada 5 líneas muestre una línea horizontal.
+
+// for (let i = 1; i <= 500; i++) {
+//   if (i % 4 === 0) {
+//     document.write(`<p>${i} (es multiplo de 4)</p>`);
+//   } else if (i % 9 === 0) {
+//     document.write(`<p>${i} (es multiplo de 9)</p>`);
+//   }
+//   if (i % 5 === 0) {
+//     document.write(`<p>${i}</p>`);
+//     document.write(`<p>---------------------------------------------</p>`);
+//   } else {
+//     document.write(`<p>${i}</p>`);
+//   }
+// }
---
+++ Js - ejercicios 3/index.js
@@ -0,0 +1,19 @@
+//////////////// Ejercicio 1 ///////////////////////////
+
+// 1- - Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+// Output: LISTA DE MESES
+
+// let listaDeMeses = [
+//   "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"];
+//    document.write(listaDeMeses.join("<br>"));
+
+/////////////// Ejercicio 2 ///////////////////
+
+// 2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+// Mostrar la longitud del arreglo.
+// Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+// Añade en última posición la ciudad de París.
+// Escribe por pantalla el elemento que ocupa la segunda posición.
+// Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
