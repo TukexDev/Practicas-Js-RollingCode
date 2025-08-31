33165645a496ba5cfd979f8aadab5726138c04cf
Matias Rearte <devmatiasrearte@gmail.com>
2025-08-31 01:52:54 -0300

    avance tp2 y tp3

--- Js - ejercicios 3/index.js
+++ Js - ejercicios 3/index.js
@@ -17,3 +17,26 @@
 // Añade en última posición la ciudad de París.
 // Escribe por pantalla el elemento que ocupa la segunda posición.
 // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+
+///////////////// Ejercicio 3 /////////////////
+///////////////// Ejercicio 4 /////////////////
+// 4- Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
+// ------------
+
+// function esParOImpar(numero) {
+//   if (numero % 2 === 0) {
+//     return "El número " + numero + " es par.";
+//   } else {
+//     return "El número " + numero + " es impar.";
+//   }
+// }
+// let num = +prompt("Ingresa un número entero:");
+
+// let resultado = esParOImpar(num);
+
+// document.write(resultado);
+
+///////////////// Ejercicio 5 /////////////////
+///////////////// Ejercicio 6 /////////////////
+///////////////// Ejercicio 7 /////////////////
+///////////////// Ejercicio 8 /////////////////
